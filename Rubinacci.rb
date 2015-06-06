aabf146eda3b9dad5117b61efbb5c26cd53efe03
Rachel Suddeth <programm_r@yahoo.com>
2015-06-06 07:57:05 -0500

    Actually change tabs to spaces

--- Rubinacci.rb
+++ Rubinacci.rb
@@ -8,7 +8,7 @@ class Rubinacci
    
    def run
       if @size < 0
-      	puts "The number of entries must be greater than zero"
+         puts "The number of entries must be greater than zero"
       end
       # If we get here, we're printing the sequence. Zero is special
       # case, since it can't use the 2 previous.
@@ -17,14 +17,13 @@ class Rubinacci
       i = 1;
       while i < @size do
         i = i + 1
-      	curr = (@p2 + @p1)
-      	print curr
-      	print ' '
-      	@p2 = @p1
-      	@p1 = curr
+         curr = (@p2 + @p1)
+         print curr
+         print ' '
+         @p2 = @p1
+         @p1 = curr
       end
-      puts (@p2 + @p1)
-      
+      puts (@p2 + @p1)     
    end
    
 end
