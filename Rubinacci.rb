3f4b57529c25c8736a43e69396013613ad393630
Rachel Suddeth <programm_r@yahoo.com>
2015-06-06 16:45:34 -0500

    Demote variables that could be local, and fix branch for bad arguments

--- Rubinacci.rb
+++ Rubinacci.rb
@@ -1,29 +1,30 @@
 class Rubinacci
-   @@size = -1
+   
    def initialize n
       @size = n.to_i
-      @p2 = 0
-      @p1 = 1
    end
    
    def run
       if @size < 0
-         puts "The number of entries must be greater than zero"
+         puts "The argument must be greater than zero"
+         return
       end
       # If we get here, we're printing the sequence. Zero is special
       # case, since it can't use the 2 previous.
+      p2 = 0
+      p1 = 1
       print 0
       print ' '
       i = 1;
       while i < @size do
         i = i + 1
-      	curr = (@p2 + @p1)
+      	curr = (p2 + p1)
       	print curr
       	print ' '
-      	@p2 = @p1
-      	@p1 = curr
+      	p2 = p1
+      	p1 = curr
       end
-      puts (@p2 + @p1)     
+      puts (p2 + p1)     
    end
    
 end
